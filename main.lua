d8a3896438225a5a5b5a34d9b3c3fa52869997b5
Shylie <fuzzcat927@gmail.com>
2023-01-07 20:05:22 -0500

    Update to use layers instead

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
-local lice = require('lib.lice.lice')
+local lice = require 'lib.lice'
 local map
 local canvas
 local curZ
@@ -7,17 +7,17 @@ function love.load()
 	love.window.setMode(640, 640, { fullscreen = false, resizable = false })
 
 	local atlas = love.graphics.newImage('tiles.png')
-	map = lice.new(9, 9, 9, atlas, 32, 32)
+	map = lice.new(9, 9, 9, atlas, 16, 16)
 
 	for x = 1, 9 do
 		for y = 1, 9 do
 			for z = 1, 9, 1 do
-				map:setTile(x, y, z, love.math.random(1, 5))
+				map:setLayerID(x, y, z, love.math.random(1, 2) == 1 and 1 or 5)
 			end
 		end
 	end
 
-	canvas = love.graphics.newCanvas(320, 320)
+	canvas = love.graphics.newCanvas(160, 160)
 	canvas:setFilter("nearest", "nearest", 0)
 
 	curZ = 1
@@ -25,16 +25,16 @@ end
 
 function love.update(dt)
 	canvas:renderTo(function()
-		love.graphics.rectangle("fill", 0, 0, 320, 320)
-		map:draw(160, 160, 9, 9, 2, 5, 5, math.floor(curZ))
+		love.graphics.rectangle("fill", 0, 0, 160, 160)
+		map:draw(80, 80, 9, 9, 1, 5, 5, math.floor(curZ))
 	end)
 
-	curZ = curZ + 2 * dt
+	curZ = curZ + dt
 	if curZ > 11 then
-		curZ = 1
+		curZ = curZ - 11
 	end
 end
 
 function love.draw()
-	love.graphics.draw(canvas, 0, 0, 0, 2, 2)
+	love.graphics.draw(canvas, 0, 0, 0, 4, 4)
 end
